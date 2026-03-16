ec3b5be6432d0054d70810d4123cca0362900e9a
Khu Tran <xuankhu1111@gmail.com>
2026-03-16 20:08:28 +0700

    Task 56D.30

--- ArtistAlbumAPI/src/main/java/com/devcamp/artistalbum/controller/ArtistController.java
+++ ArtistAlbumAPI/src/main/java/com/devcamp/artistalbum/controller/ArtistController.java
@@ -4,6 +4,7 @@ import com.devcamp.artistalbum.model.Artist;
 import com.devcamp.artistalbum.service.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,14 @@ public class ArtistController {
     @GetMapping("/artist-info")
     public Artist getArtistInfo(@RequestParam int artistId) {
         return artistService.getArtistById(artistId);
+    }
+
+    @GetMapping("/artists/{index}")
+    public Artist getArtistByIndex(@PathVariable("index") int index) {
+        ArrayList<Artist> allArtists = artistService.getAllArtists();
+        if (index >= 0 && index < allArtists.size()) {
+            return allArtists.get(index);
+        }
+        return null;
     }
 }
--- ArtistAlbumAPI/src/main/java/com/devcamp/artistalbum/service/ArtistService.java
+++ ArtistAlbumAPI/src/main/java/com/devcamp/artistalbum/service/ArtistService.java
@@ -35,6 +35,30 @@ public class ArtistService {
             new Album(303, "Changes", new ArrayList<>(Arrays.asList("Yummy", "Intentions")))
         ));
         artists.add(new Artist(3, "Justin Bieber", justinAlbums));
+        
+        // Artist 4: Ed Sheeran (56D.30 update)
+        ArrayList<Album> edAlbums = new ArrayList<>(Arrays.asList(
+            new Album(401, "Divide", new ArrayList<>(Arrays.asList("Shape of You", "Perfect"))),
+            new Album(402, "Multiply", new ArrayList<>(Arrays.asList("Sing", "Thinking Out Loud"))),
+            new Album(403, "Equals", new ArrayList<>(Arrays.asList("Bad Habits", "Shivers")))
+        ));
+        artists.add(new Artist(4, "Ed Sheeran", edAlbums));
+
+        // Artist 5: Drake (56D.30 update)
+        ArrayList<Album> drakeAlbums = new ArrayList<>(Arrays.asList(
+            new Album(501, "Scorpion", new ArrayList<>(Arrays.asList("God's Plan", "In My Feelings"))),
+            new Album(502, "Views", new ArrayList<>(Arrays.asList("Hotline Bling", "One Dance"))),
+            new Album(503, "Certified Lover Boy", new ArrayList<>(Arrays.asList("Way 2 Sexy", "Fair Trade")))
+        ));
+        artists.add(new Artist(5, "Drake", drakeAlbums));
+
+        // Artist 6: Adele (56D.30 update)
+        ArrayList<Album> adeleAlbums = new ArrayList<>(Arrays.asList(
+            new Album(601, "30", new ArrayList<>(Arrays.asList("Easy On Me", "Oh My God"))),
+            new Album(602, "25", new ArrayList<>(Arrays.asList("Hello", "Send My Love"))),
+            new Album(603, "21", new ArrayList<>(Arrays.asList("Rolling in the Deep", "Someone Like You")))
+        ));
+        artists.add(new Artist(6, "Adele", adeleAlbums));
 
         return artists;
     }
